fec28803548edc13f842bc91a95d77bffdc62345
Anderson Gomes <andersomyoshi@hotmail.com>
2025-10-15 13:55:51 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
-// CORREÇÃO: A importação do getFirestore foi adicionada.
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Seu código de configuração, que está correto.
 const firebaseConfig = {
   apiKey: "AIzaSyDESJdxOC4v29_9AUMdwKhH_qV9eJiDoRE",
   authDomain: "yoshisbar-253e1.firebaseapp.com",
   projectId: "yoshisbar-253e1",
-  storageBucket: "yoshisbar-253e1.appspot.com", // Corrigido de .firebasestorage.app para .appspot.com
+  storageBucket: "yoshisbar-253e1.appspot.com",
   messagingSenderId: "737401455071",
   appId: "1:737401455071:web:78c8663a69d79ea725c46a",
   measurementId: "G-PYWXBT90LX"
