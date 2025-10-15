faad5050ef150f64f6e2f95221f5db0c4c9442f7
Anderson Gomes <andersomyoshi@hotmail.com>
2025-10-15 13:18:43 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDESJdxOC4v29_9AUMdwKhH_qV9eJiDoRE",
   authDomain: "yoshisbar-253e1.firebaseapp.com",
   projectId: "yoshisbar-253e1",
-  storageBucket: "yoshisbar-253e1.appspot.com",
+  storageBucket: "yoshisbar-253e1.firebasestorage.app",
   messagingSenderId: "737401455071",
   appId: "1:737401455071:web:78c8663a69d79ea725c46a",
   measurementId: "G-PYWXBT90LX"
@@ -13,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
